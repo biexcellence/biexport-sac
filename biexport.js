fe812e53c3f0498bee58efb84a80b1e40d680c6e
Thilo Knötzele <thilo.knoetzele@biexcellence.com>
2020-02-15 21:21:06 +0100

    syntax error

--- biexport.js
+++ biexport.js
@@ -349,10 +349,10 @@
                                             this._export_settings.application_array = [];
                                             this._export_settings.application_array.push({ "application": context.getAppArgument().appId });
 
-                                            let objIndex = -1;
-                                            if (this._export_settings.array_var != "") {
-                                                objIndex = this._export_settings.array_var.findIndex(v => v.parameter == oEvent.getParameter("id").replace("_value", ""));
+                                            if (!this._export_settings.array_var) {
+                                                this._export_settings.array_var = [];
                                             }
+                                            let objIndex = this._export_settings.array_var.findIndex(v => v.parameter == oEvent.getParameter("id").replace("_value", ""));
                                             if (objIndex > -1) {
                                                 this._export_settings.array_var[objIndex].value = oEvent.getParameter("value");
                                             } else {
@@ -373,10 +373,10 @@
                                             this._export_settings.application_array = [];
                                             this._export_settings.application_array.push({ "application": context.getAppArgument().appId });
 
-                                            let objIndex = -1;
-                                            if (this._export_settings.array_var != "") {
-                                                objIndex = this._export_settings.array_var.findIndex(v => v.parameter == oEvent.getParameter("id").replace("_iterative", ""));
+                                            if (!this._export_settings.array_var) {
+                                                this._export_settings.array_var = [];
                                             }
+                                            let objIndex = this._export_settings.array_var.findIndex(v => v.parameter == oEvent.getParameter("id").replace("_iterative", ""));                     
                                             if (objIndex > -1) {
                                                 this._export_settings.array_var[objIndex].iterative = oEvent.getParameter("selected");
                                             } else {
