a8e189150f2097446c403abc4c704e70c06c7c67
Thilo Knötzele <thilo.knoetzele@biexcellence.com>
2020-04-30 17:02:44 +0200

    try not to reuse dialog object

--- aps_binotification.js
+++ aps_binotification.js
@@ -78,39 +78,40 @@
                 }
             });
 
+            let dialog = new sap.m.Dialog({
+                title: "Edit HTML Notification",
+                contentWidth: "600px",
+                contentHeight: "400px",
+                draggable: true,
+                resizable: true,
+                content: [
+                    textEditor
+                ],
+                beginButton: new sap.m.Button({
+                    text: "Submit",
+                    press: () => {
+                        let value = textEditor.getValue();
+                        let properties = {};
+                        this[id] = properties[id] = value;
+                        this._firePropertiesChanged(properties);
+                        dialog.close();
+                    }
+                }),
+                endButton: new sap.m.Button({
+                    text: "Cancel",
+                    press: () => {
+                        dialog.close();
+                    }
+                }),
+                afterClose: () => {
+                    textEditor.destroy();
+                    dialog.destroy();
+                }
+            });
+
             let link = new sap.m.Link({
                 text: "Edit HTML Notification...",
                 press: oEvent => {
-                    let dialog = new sap.m.Dialog({
-                        title: "Edit HTML Notification",
-                        contentWidth: "600px",
-                        contentHeight: "400px",
-                        draggable: true,
-                        resizable: true,
-                        content: [
-                            textEditor
-                        ],
-                        beginButton: new sap.m.Button({
-                            text: "Submit",
-                            press: () => {
-                                let value = textEditor.getValue();
-                                let properties = {};
-                                this[id] = properties[id] = value;
-                                this._firePropertiesChanged(properties);
-                                dialog.close();
-                            }
-                        }),
-                        endButton: new sap.m.Button({
-                            text: "Cancel",
-                            press: () => {
-                                dialog.close();
-                            }
-                        }),
-                        afterClose: () => {
-                            textEditor.destroy();
-                            dialog.destroy();
-                        }
-                    });
 
                     dialog.open();
 
