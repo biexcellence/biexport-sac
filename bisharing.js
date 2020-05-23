05dbd920b01273c507c4cca8c36e60fc4e59a460
Thilo Knötzele <thilo.knoetzele@biexcellence.com>
2020-05-23 13:44:01 +0200

    switch from addHeaderParameter to addParameter (adds a hidden input field) for a trial

--- bisharing.js
+++ bisharing.js
@@ -294,6 +294,8 @@
             }
         );
 
+        lupload.addParameter(new sap.m.UploadCollectionParameter({ name: "bie_openbi_export_settings_json", value: JSON.stringify(this._sharing_settings) }));
+
             let ldialog = new sap.m.Dialog({
                 title: "Upload files",
                 endButton: new sap.m.Button({
