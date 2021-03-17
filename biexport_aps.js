3be4f9589ff982cd3609528c87dd21aeb1562bdc
thiloknoetzele <60661436+thiloknoetzele@users.noreply.github.com>
2021-03-17 11:32:49 +0100

    Update biexport_aps.js
    eliminate typo

--- biexport_aps.js
+++ biexport_aps.js
@@ -383,7 +383,7 @@ debugger;
 
                     });
 
-                    filterList.                         list.setModel(model);
+                    filterList.setModel(model);
                     filter.addList(filterList);
 
                });
