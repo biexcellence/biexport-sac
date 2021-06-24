6146bc6edc1b76d48cdec56ae8967991ad605ac6
Thilo Knötzele <thilo.knoetzele@BIeXcellenceSoftware.onmicrosoft.com>
2021-06-24 19:22:33 +0200

    back to iframe

--- bisharing.js
+++ bisharing.js
@@ -210,12 +210,12 @@
                 "&link=" + encodeURIComponent(this._connectParams["entryLink"]) +
                 "&navigate=" + encodeURIComponent(this._connectParams["showNavigation"]) +
                 "&origin=" + encodeURIComponent(location.origin));
-            // this._shadowRoot.appendChild(liframe);
+             this._shadowRoot.appendChild(liframe);
 
             //let lscript = document.createElement("script");
             //lscript.setAttribute("src", this._sharing_settings.server_urls + "/export_resources/OneDrive.js")
             //this._shadowRoot.appendChild(lscript);
-            this._launchOneDrivePicker(this._connectParams["clientId"], this._connectParams["entryLink"], (this._connectParams["showNavigation"] == 'false'));
+            //this._launchOneDrivePicker(this._connectParams["clientId"], this._connectParams["entryLink"], (this._connectParams["showNavigation"] == 'false'));
             //var odOptions = {
             //    clientId: this._connectParams["clientId"],
             //    action: "share",
