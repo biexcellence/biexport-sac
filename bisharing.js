b141472c0cbd2ab2672b59402f263eb6d758ff69
Thilo Knötzele <thilo.knoetzele@BIeXcellenceSoftware.onmicrosoft.com>
2021-06-24 18:49:56 +0200

    bug

--- bisharing.js
+++ bisharing.js
@@ -214,7 +214,7 @@
 
             //let lscript = document.createElement("script");
             //lscript.setAttribute("src", this._sharing_settings.server_urls + "/export_resources/OneDrive.js")
-            this._shadowRoot.appendChild(lscript);
+            //this._shadowRoot.appendChild(lscript);
             this._launchOneDrivePicker(this._connectParams["clientId"], this._connectParams["entryLink"], (this._connectParams["showNavigation"] == 'false'));
             //var odOptions = {
             //    clientId: this._connectParams["clientId"],
