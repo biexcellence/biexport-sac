62cf14fc09b14683ed5d09bc75e4343e960bb84d
campersau <buchholz.bastian@googlemail.com>
2023-12-15 20:51:26 +0100

    [biexport] add text type for Texts

--- biexport_aps.js
+++ biexport_aps.js
@@ -330,15 +330,16 @@
             modelFilters["Tables"] = [new sap.ui.model.Filter("type", sap.ui.model.FilterOperator.EQ, "table")];
             modelFilters["Charts"] = [new sap.ui.model.Filter("type", sap.ui.model.FilterOperator.EQ, "viz"), new sap.ui.model.Filter("type", sap.ui.model.FilterOperator.EQ, "viz2")];
             modelFilters["Layouts"] = [new sap.ui.model.Filter("type", sap.ui.model.FilterOperator.EQ, "pagebook"), new sap.ui.model.Filter("type", sap.ui.model.FilterOperator.EQ, "panel"), new sap.ui.model.Filter("type", sap.ui.model.FilterOperator.EQ, "flowpanel"), new sap.ui.model.Filter("type", sap.ui.model.FilterOperator.EQ, "tabstrip")];
-            modelFilters["Texts"] = [new sap.ui.model.Filter("type", sap.ui.model.FilterOperator.EQ, "textWidget"), new sap.ui.model.Filter("type", sap.ui.model.FilterOperator.EQ, "inputField"), new sap.ui.model.Filter("type", sap.ui.model.FilterOperator.EQ, "textArea")];
+            modelFilters["Texts"] = [new sap.ui.model.Filter("type", sap.ui.model.FilterOperator.EQ, "text"), new sap.ui.model.Filter("type", sap.ui.model.FilterOperator.EQ, "textWidget"), new sap.ui.model.Filter("type", sap.ui.model.FilterOperator.EQ, "inputField"), new sap.ui.model.Filter("type", sap.ui.model.FilterOperator.EQ, "textArea")];
             modelFilters["Filters"] = [new sap.ui.model.Filter("type", sap.ui.model.FilterOperator.EQ, "filterLine"), new sap.ui.model.Filter("type", sap.ui.model.FilterOperator.EQ, "dropdownBox")];
             modelFilters["Others"] = new sap.ui.model.Filter({
                 filters: [
                     new sap.ui.model.Filter("type", sap.ui.model.FilterOperator.NE, "sdk_com_biexcellence_openbi_sap_sac_export__0"),
                     new sap.ui.model.Filter("type", sap.ui.model.FilterOperator.NE, "pagebook"), new sap.ui.model.Filter("type", sap.ui.model.FilterOperator.NE, "panel"), new sap.ui.model.Filter("type", sap.ui.model.FilterOperator.NE, "flowpanel"), new sap.ui.model.Filter("type", sap.ui.model.FilterOperator.NE, "tabstrip"),
-                    new sap.ui.model.Filter("type", sap.ui.model.FilterOperator.NE, "textWidget"), new sap.ui.model.Filter("type", sap.ui.model.FilterOperator.NE, "inputField"), new sap.ui.model.Filter("type", sap.ui.model.FilterOperator.NE, "textArea"),
-                    new sap.ui.model.Filter("type", sap.ui.model.FilterOperator.NE, "filterLine"), new sap.ui.model.Filter("type", sap.ui.model.FilterOperator.NE, "dropdownBox"),
-                    new sap.ui.model.Filter("type", sap.ui.model.FilterOperator.NE, "table"), new sap.ui.model.Filter("type", sap.ui.model.FilterOperator.NE, "viz"), new sap.ui.model.Filter("type", sap.ui.model.FilterOperator.NE, "viz2")
+                    new sap.ui.model.Filter("type", sap.ui.model.FilterOperator.NE, "table"),
+                    new sap.ui.model.Filter("type", sap.ui.model.FilterOperator.NE, "viz"), new sap.ui.model.Filter("type", sap.ui.model.FilterOperator.NE, "viz2"),
+                    new sap.ui.model.Filter("type", sap.ui.model.FilterOperator.NE, "text"), new sap.ui.model.Filter("type", sap.ui.model.FilterOperator.NE, "textWidget"), new sap.ui.model.Filter("type", sap.ui.model.FilterOperator.NE, "inputField"), new sap.ui.model.Filter("type", sap.ui.model.FilterOperator.NE, "textArea"),
+                    new sap.ui.model.Filter("type", sap.ui.model.FilterOperator.NE, "filterLine"), new sap.ui.model.Filter("type", sap.ui.model.FilterOperator.NE, "dropdownBox")
                 ],
                 and: true
             });
